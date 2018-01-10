7c1e2807a9e7a579158894dc3918c18806e6d779
deltaalpha33 <dalexakos@gmail.com>
2018-01-09 20:15:26 -0500

    added round generation

--- OrcMathGUI/src/simonGame/GameScreen.java
+++ OrcMathGUI/src/simonGame/GameScreen.java
@@ -22,6 +22,8 @@ public class GameScreen extends FullFunctionScreen {
 	private Button pinkButton;
 	private Button orangeButton;
 	
+	private boolean wonGame = false;
+	private boolean endGame = false;
 	
 	
 	int seqPos = 0;
@@ -29,50 +31,101 @@ public class GameScreen extends FullFunctionScreen {
 
 	public GameScreen(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		
+		//initialize first round
+		
+		currentRound = new String[3];
+		fillArrayWithMoves(currentRound);
+		
+		displayCurrentRound();
 	}
 	
 	public void buttonClick(String color) {
 
+		if(this.currentRound[this.seqPos] == color) {
+			this.seqPos++;
+		}
+		else {
+			System.out.println("end game");
+		}
 		
-		
+		if(this.seqPos == this.currentRound.length) {
+			setUpNextRound();
+			displayCurrentRound();
+		}
+
 	}
 	
 	
-	public String getRandMove() {
+	public void setUpNextRound() {
+		String[] nextRound = new String[this.currentRound.length+1];
+		System.arraycopy( this.currentRound, 0, nextRound, 0, this.currentRound.length );
+		
+		if(nextRound.length >= 2) {
+			nextRound[nextRound.length-1] = getRandMove(nextRound[nextRound.length-2]);
+		}else
+		{
+			nextRound[nextRound.length-1] = getRandMove("None");
+		}
+		
+		
+		
+		this.currentRound = nextRound;
+		this.seqPos = 0;
+		
+	}
+	
+	public void displayCurrentRound() {
+		System.out.println("current round:");
+		for(String s : this.currentRound) {
+			System.out.println(s);
+		}
+		
+	}
+
+	public void fillArrayWithMoves(String[] arr) {
+		if(arr.length > 0) {
+			arr[0] = getRandMove("None");
+		}
+		for(int i = 1; i < arr.length; i++) {
+			arr[i] = getRandMove(arr[i-1]);
+		}
+	}
+	
+	public String getRandMove(String prevColor) {
 		while(true) {
+			
 			int randNum = (int)(Math.random() * 6);
 			
 			switch (randNum){
 			case 0:
-				if(currentRound.length>0 && currentRound[currentRound.length-1] != "red") {
+				if(prevColor != "red") {
 					return "red";
 				}
 				
 			case 1:
-				if(currentRound.length>0 && currentRound[currentRound.length-1] != "yellow") {
+				if(prevColor != "yellow") {
 					return "yellow";
 				}
 				
 			case 2:
-				if(currentRound.length>0 && currentRound[currentRound.length-1] != "green") {
+				if(prevColor != "green") {
 					return "green";
 				}
 				
 			case 3:
-				if(currentRound.length>0 && currentRound[currentRound.length-1] != "pink") {
+				if(prevColor != "pink") {
 					return "pink";
 				}
 				
 			case 4:
-				if(currentRound.length>0 && currentRound[currentRound.length-1] != "orange") {
+				if(prevColor != "orange") {
 					return "orange";
 				}
 			case 5:
-				if(currentRound.length>0 && currentRound[currentRound.length-1] != "cyan") {
+				if(prevColor != "cyan") {
 					return "cyan";
 			}
-		
 		}
 		}
 	}
